017095cfbfdce416b80e604057283f7bc4368008
Sumukha87 <73688740+Sumukha87@users.noreply.github.com>
2023-04-02 20:28:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import base64
+
+from flask import Flask,render_template,request 
+import qrcode
+from io import BytesIO
+from base64 import b64encode
+
+app = Flask(__name__, template_folder='template')
+
+@app.route('/')
+
+def home():
+    return render_template('index.html')
+
+@app.route('/',methods=['POST'])
+
+def generateQR():
+    memory = BytesIO()
+    data = request.form.get('link')
+    img = qrcode.make(data)
+    img.save(memory)
+    memory.seek(0)
+
+    base64_img = 'data:image/png;base64,'+ b64encode(memory.getvalue()).decode('ascii')
+    return render_template('index.html',data=base64_img)
+
+if __name__=='__main__':
+    app.run(debug=True)
